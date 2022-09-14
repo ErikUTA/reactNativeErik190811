d9d5bcde9ce9aa4527359c336b0994d73d274a2d
ErikUTA <erik4tohj@gmail.com>
2022-09-13 23:30:24 -0500

    feat: added new changes to display full name

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, Image, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [inputName, setInputName] = useState("");
+  const [inputLastName, setInputLastName] = useState("");
+  const [fullName, setFullName] = useState("");
+
+  const datos = () => {
+    inputName !== "" && inputLastName !== "" ? setFullName(`Hola ${inputName} ${inputLastName}`) : setFullName("");
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Erik Roman Romero Flores</Text>
-      <StatusBar style="auto" />
+      <Image source={require('./assets/react.png')}/>
+      {
+        !!!fullName ?
+          <Text style={styles.text}>Ingresar datos</Text>
+          :
+          <Text style={styles.text}>{fullName}</Text>
+      }
+      <StatusBar backgroundColor='red' />
+      <TextInput 
+        style={styles.inputText}
+        placeholder='Nombre'
+        onChangeText={(e) => setInputName(e)}
+      />
+      <TextInput 
+        style={styles.inputText}
+        placeholder='Apellido'
+        onChangeText={(e) => setInputLastName(e)}
+      />
+      <Button 
+        title='Enviar'
+        onPress={() => {!!inputName && !!!inputLastName ? setFullName("Ingresa apellidos") : !!inputLastName && !!!inputName ? setFullName("Ingresa tu nombre") : !!!inputName && !!!inputName ? setFullName("Ingresa tus datos") : datos()}}
+      />
     </View>
   );
 }
@@ -13,11 +43,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'lightgreen',
+    backgroundColor: 'darkslategray',
     alignItems: 'center',
     justifyContent: 'center',
   },
   text: {
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    color: 'white',
+    margin: '2%'
+  },
+  inputText: {
+    color: 'black',
+    fontWeight: 'bold',
+    backgroundColor: 'white',
+    width: '80%',
+    margin: '2%'
   }
 });
